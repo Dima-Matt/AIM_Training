98626040ed4cb81d9f04d83e4d855f962476b846
Dima Mironov <mattmars9548@gmail.com>
2024-06-03 21:26:31 +0300

    html and css of the game

---
+++ main.js
@@ -0,0 +1,8 @@
+const startBtn = document.getElementById('id');
+const cards = document.querySelectorAll('.page');
+const timeController = document.querySelectorAll('.time-list__item button');
+const modal = document.getElementById('modal');
+const board = document.getElementById('board');
+const scoreEl = document.getElementById('score');
+const time = document.getElementById('time');
+
